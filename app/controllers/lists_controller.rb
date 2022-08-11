9d99b8abf6ae214e6c5f6bf0d0955ce03df7b39f
Josh Chappelow <jdchappelow@gmail.com>
2022-08-11 16:19:11 +0900

    time for css

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,32 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, :improcessable_entity
+    end
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  # get 'bookmarks/new'
+  # get 'bookmarks/show'
+  # get 'lists/index'
+  # get 'lists/create'
+  # get 'lists/new'
+  # get 'lists/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+    resources :lists, only: [:index, :create, :new, :show] do
+      resources :bookmarks, only: [:new, :create]
+    end
+    resources :bookmarks, only: [:destroy]
 end
